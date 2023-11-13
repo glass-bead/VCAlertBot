0b7aa53110629fac3554c922b3d7335a630ab79f
Carolina Veloso <carolina.mg.veloso@gmail.com>
2023-11-13 18:57:05 +0100

    Check when member joins VC

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js'
 const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+    partials: [Partials.Channel, Partials.Message],
+});
 
 // Loading command files
 client.commands = new Collection();
@@ -28,6 +39,22 @@ client.login(TOKEN);
 // Bot is online
 client.on('ready', () => {
     console.log(`${client.user.tag} is online.`);
+});
+
+
+// Checks if member joins a VC
+client.on('voiceStateUpdate', async (oldState, newState) => {
+
+    var oldVoice = oldState.channelId;
+    var newVoice = newState.channelId;
+
+    // When a user joins a VC
+    if (newVoice != null) {
+        if (oldVoice == null, oldVoice != newVoice) {
+            var userID = newState.id;
+            console.log(`The user ${userID} joined a voice chat.`);
+        }
+    }
 });
 
 // Listen for user interactions with bot
